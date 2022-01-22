243d41b13ac517ea0d1a153b44a385772cad851a
Seagull <ssean5739@gmail.com>
2022-01-22 15:08:36 +0200

    Switched redux.js to redux.ts

--- frontend/src/store/redux.js
+++ frontend/src/store/redux.js
@@ -1,44 +1,41 @@
-import {createSlice, configureStore} from '@reduxjs/toolkit';
-
-const initialState = {
-  isModalWindowShown: false,
-  modalWindowTaskText: '',
-  filteredTasksToShow: [],
-  modalWindowTaskId: null,
-  modalWindowAction: null,
-  isLoaderShown: false
-}
-
-const stateSlice = createSlice({
-  name: 'stateSlice',
-  initialState,
-  reducers: {
-    setModalWindowVisibility(state, action) {
-      state.isModalWindowShown = action.payload;
-    },
-    setModalWindowTaskText(state, action) {
-      state.modalWindowTaskText = action.payload;
-    },
-    setFilteredTasksToShow(state, action) {
-      state.filteredTasksToShow = action.payload;
-    },
-    setModalWindowTaskId(state, action) {
-      state.modalWindowTaskId = action.payload;
-    },
-    setModalWindowAction(state, action) {
-      state.modalWindowAction = action.payload;
-    },
-    setLoaderVisibility(state, action) {
-      state.isLoaderShown = action.payload;
+"use strict";
+exports.__esModule = true;
+exports.actions = void 0;
+var toolkit_1 = require("@reduxjs/toolkit");
+var initialState = {
+    isModalWindowShown: false,
+    modalWindowTaskText: '',
+    filteredTasksToShow: [],
+    modalWindowTaskId: null,
+    modalWindowAction: null,
+    isLoaderShown: false
+};
+var stateSlice = (0, toolkit_1.createSlice)({
+    name: 'stateSlice',
+    initialState: initialState,
+    reducers: {
+        setModalWindowVisibility: function (state, action) {
+            state.isModalWindowShown = action.payload;
+        },
+        setModalWindowTaskText: function (state, action) {
+            state.modalWindowTaskText = action.payload;
+        },
+        setFilteredTasksToShow: function (state, action) {
+            state.filteredTasksToShow = action.payload;
+        },
+        setModalWindowTaskId: function (state, action) {
+            state.modalWindowTaskId = action.payload;
+        },
+        setModalWindowAction: function (state, action) {
+            state.modalWindowAction = action.payload;
+        },
+        setLoaderVisibility: function (state, action) {
+            state.isLoaderShown = action.payload;
+        }
     }
-  }
 });
-
-const store = configureStore({
-  reducer: stateSlice.reducer
-})
-
-export const actions = stateSlice.actions;
-export default store;
-
-
+var store = (0, toolkit_1.configureStore)({
+    reducer: stateSlice.reducer
+});
+exports.actions = stateSlice.actions;
+exports["default"] = store;
